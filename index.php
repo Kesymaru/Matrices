b7623dbd7c04f5093e3e97a9ed728ac9a98afb43
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-10-17 11:32:33 -0600

    Index
    version desarrollo inestable

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+	
+*/
+
+//base de datos
+require_once ("db.php");
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>77Digital</title>
+	<meta charset="utf-8" />
+	
+	<link rel="stylesheet" href="css/style.css" TYPE="text/css" MEDIA=screen>	
+
+</head>
+
+<body>
+
+	
+
+
+</body>
+
+</html>
