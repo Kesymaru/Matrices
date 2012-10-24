443307dfa6d8cea3691c65773319ab54a799d69f
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-10-24 13:37:49 -0600

    maquetacion
    maquetacion segun diseno

--- index.php
+++ index.php
@@ -1,105 +1,89 @@
+<?php 
+	require_once("db.php");
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
-	<title>Registro</title>
+	<title>Escala</title>
 	<meta charset="utf-8" />
 	
 	<link rel="stylesheet" href="css/style.css" TYPE="text/css">
-	<link rel="stylesheet" href="css/formulario.css" TYPE="text/css">
-	<link rel="stylesheet" href="css/jquery.tagsinput.css" TYPE="text/css">
 	<link href="css/jquery-ui-1.9.0.custom.css" rel="stylesheet">
 
 	<script src="js/jquery-1.8.2.js"></script>
-	<script src="js/jquery-ui-1.9.0.custom.js"></script>
-	<script src="js/jquery.tagsinput.js"></script>
+	<script src="/js/jquery.masonry.min.js"></script>
+	<!-- <script src="js/jquery-ui-1.9.0.custom.js"></script> -->
 	<script src="js/main.js"></script>
 </head>
 
 <body>
 
-<?php
-/*
-	
-*/
+	<header>
+		<img src="images/logo.png" class="logo">
 
-//base de datos
-require_once("bd.php");
-
-session_start();
-
-
-if(loguado()){
-	echo '<div id="contenido"></div>';
-
-	echo '<SCRIPT TYPE="text/javascript">
-	contenido(\'loginbox\')
-	</SCRIPT>';
-}else{
-
-}
-?>
-
-<div class="barraProgreso">
-
-</div>
-<form id="formulario">
-	<hr><h1>Datos Proyecto</h1><hr>
-
-	<div class="contenedor">
-		<div class="left">
-			Nombre:<br/>
-			<input id="nombre" type="text"  required="required" placeholder="Nombre">
-			<br/>
-			Fecha:<br/>
-			<input type="text" id="datepicker" />
-		</div>
-		<div class="right">
-			<div class="categoria">
-				Categoria<br/>
-				<?php 
-					categorias();
-				?>
+		<div class="toolbar">
+			<div id="cliente">
+				<span>Cliente Nombre</span>
+				<ul id="proyectos">
+					<li>
+						Proyectos
+					</li>
+				</ul>
 			</div>
-			<div class="sub-categoria">
-				Sub-categoria<br/>
-				<?php 
-					subcategorias();
-				?>
-			</div>
-			<div class="sub-categoria">
-				Sub-sub-categoria<br/>
-				<?php 
-					subcategorias();
-				?>
+			<div id="search">
+				<input type="text" placeholder="hacer busqueda" required="requiered">
+				<button>Send</button>
 			</div>
 		</div>
-	</div>
 
-	<div class="bottom">
-		<input id="cats" type="text" class="tags" value="" />
-		<input id="tags" type="text" class="tags" value="" />
-		Notas<br/>
-		<textarea placeholder="Notas"></textarea>
-	</div>
-	
-	<button onClick="imprimir()">Seguiente</button>
-</form>
 
+	</header>
+
+	<div id="main">
+		
+		<div id="menu">
+			<?php
+				menu();
+			?>
+		</div><!-- end menu -->
+
+		<div id="content">
+
+			<div id="nivel1">
+				<div id="categoriaTitulo">
+					<div>TODO categoria Titulo</div>
+				</div>
+				<div id="subCategorias">
+					<div>TODO ajac para subcategorias de Categoria</div>
+				</div>
+			</div><!-- end nivel 1-->
+
+			<div id="nivel2">
+				<div id="columna1">
+					<div id="descripcion">
+						<div class="titulo">
+							TODO titulo ajax categoria
+						</div>
+						<div>
+							TODO descripcion
+						</div>
+					</div>
+				</div>
+
+				<div id="columna2">
+					<div>
+						TODO ajax para mostrar informacion de subcategorias<br/>
+						TODO mansory para acomodar las columnas
+					</div>
+				</div>
+
+			</div><!-- end nivel 2-->
+
+		</div><!-- end content -->
+
+	</div><!-- end main -->
 
 </body>
 
 </html>
-
-<?php
-
-//determina si el usuario esta logueado
-function loguado(){
-	if( !isset($_SESSION['logueado']) ) {
-		return false;
-	}else{
-		return true;
-	}
-}
-
-?>
