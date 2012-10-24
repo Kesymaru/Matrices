f0017c533939da99767b4618bb5e4385d2ff1f40
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-10-24 13:36:58 -0600

    main.js
    TODO:
    +ajax para cargar datos de la categoria seleccion
    +ajax para cargar subcategorias
    +ajax para cargar info de subcategorias selecionadas

--- js/main.js
+++ js/main.js
@@ -1,86 +1,16 @@
 
 $(document).ready(function(){
 
-	$( "#accordion" ).accordion();
-
-	$( "button" ).button();
-
-	$( "#datepicker" ).datepicker();
-
-	$('#tags').tagsInput({
-	   'height':'100px',
-	   'width':'300px',
-	   'interactive':false,
-	   'defaultText':'Agregar',
-	   'removeWithBackspace' : true,
-	   'placeholderColor' : '#666666'
-	});
-
-	$('#cats').tagsInput({
-	   'height':'100px',
-	   'width':'300px',
-	   'interactive':false,
-	   'defaultText':'Agregar',
-	   'removeWithBackspace' : true,
-	   'placeholderColor' : '#666666'
-	});
 });
 
-//carga el contenido
-function contenido(func){
-	var queryParams = { "func" : func};
-	var cont = $("#contenido");
-	  	$.ajax({
-	        data:  queryParams,
-	        url:   'ajax.php',
-	        type:  'post',
-	        beforeSend: function () {
-	                cont.html('<img class="loader" src="http://77digital.com/Desarrollo/dipo/images/loader.gif" alt="cargando" />');
-	        },
-	        success:  function (response) { 
-	        		cont.load(response);
-	        }
-		});
-}
-
-//carga el formulario
-function formulario(func){
-	var queryParams = { "func" : func};
-	var cont = $("#formulario");
-	  	$.ajax({
-	        data:  queryParams,
-	        url:   'ajax.php',
-	        type:  'post',
-	        beforeSend: function () {
-	                cont.html('<img class="loader" src="http://77digital.com/Desarrollo/dipo/images/loader.gif" alt="cargando" />');
-	        },
-	        success:  function (response) { 
-	        		cont.load(response);
-	        }
-		});
-}
-
-//quita y pone categoria seleccionada
-function categoria(etiqueta){
-	if ($('#cats').tagExist(etiqueta)) { 
-
-		$('#cats').removeCat(etiqueta);
-
-	}else{
-
-		$('#cats').addCat(etiqueta);
-	}
-}
-
-//quita y pone las etiquetas seleccionadas
-function etiqueta(etiqueta){
-	if ($('#tags').tagExist(etiqueta)) { 
-
-		$('#tags').removeTag(etiqueta);
-
-	}else{
-
-		$('#tags').addTag(etiqueta);
-
-	}
+//menu
+function menu(id){
+	var menu = $('#menu'+id);
+	$('#menu ul').children('li').removeClass('seleccionada');
+	menu.css({
+		'opacity':'0',
+	});
+	menu.addClass('seleccionada').animate({
+		'opacity':1,
+	},1000);
 }
