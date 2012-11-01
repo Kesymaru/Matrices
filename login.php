8a9de609dacc03571909b6e48424f126d310ef85
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-11-01 10:08:29 -0600

    login v 0.8
    mejoras, orden codigo, reseteo de password, animacion

--- login.php
+++ login.php
@@ -20,72 +20,189 @@ if( isset($_SESSION['logueado']) ){
 	<title>Escala Login</title>
 	
 	<meta charset="utf-8">
-	<link rel="shortcut icon" href="images/favicon.ico"> 
+	<link rel="shortcut icon" href="/favicon.ico"> 
 
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<link rel="stylesheet" href="css/jquery-ui-1.9.0.custom.css" type="text/css">
 	<link rel="stylesheet" href="css/validationEngine.jquery.css" type="text/css">
 
-	<!-- <script src="js/jquery-1.8.2.js"></script> -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+	<script src="js/jquery-1.8.2.js"></script>
 	<script src="js/jquery-ui-1.9.0.custom.js"></script>
-
+	
+	<!-- validacion de form -->
 	<script src="js/languages/jquery.validationEngine-es.js" type="text/javascript" charset="utf-8"></script>
 	<script src="js/jquery.validationEngine.js" type="text/javascript" charset="utf-8"></script>
-	<script src="js/jquery.placeholder.js"></script>
+
+	<!-- placeholder para ie -->
+	<script src="js/jquery.placeholder.js" type="text/javascript"></script>
+
+	<!-- notificaciones -->
+	<script src="js/noty/jquery.noty.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/noty/layouts/topCenter.js"></script>
+	<script type="text/javascript" src="js/noty/themes/default.js"></script>
 
 	<script type="text/javascript">
 
     $(document).ready(function(){
-
+    	
     	$( "input[type=submit], button, span" ).button();
 
         $('#nuevoUsuario').hide();
-
-        $('#entrar').click(function(){
-        	logIn();
-        });
+        $('#formRecuperacion').hide();
+        $('#resetear').hide();
 
         $("#formID").validationEngine();
         $('input[placeholder]').placeholder();
+
+        /*$('#entrar').click(function(){
+        	logIn();
+        });*/
+
+        /*$('#recuperacion').submit(function() {
+			return false;
+		});*/
+
    	});
 
 	function loginbox(cambio){
 
-		if( cambio == 2 ){
-			$('#nuevoUsuario').fadeOut(1000,function(){
-				$('#usuarios').fadeIn();
-			});
+		if(cambio == 2){
+			$('#nuevoUsuario').fadeOut(1000,function(){$('#usuarios').fadeIn();});
 		}else{
-			$('#usuarios').fadeOut(1000,function(){
-				$('#nuevoUsuario').fadeIn();
-			});
+			$('#usuarios').fadeOut(1000,function(){$('#nuevoUsuario').fadeIn();});
 		}
 		
 	}
 
+	function formRecuperacion(){
+
+		if( $('#formRecuperacion').is(':visible')){
+			$('#formRecuperacion').slideUp(500);
+			$('#login').slideDown(500);
+
+			$('#resetear').fadeOut(500, function(){ $('#entrar').fadeIn(500); });
+		}else{
+			$('#formRecuperacion').slideDown(500);
+			$('#login').slideUp(500,function(){ $('#login').hide();});
+
+			$('#entrar').fadeOut(500, function(){ $('#resetear').fadeIn(500); });
+		}
+
+	}
+
 	function logIn(){
 
-	var usuario = $('#usuario').val();
-	var password = $('#password').val();
+		if ( $('#formID').validationEngine('validate') ){
 
-	var queryParams = { "func" : 'logIn', "usuario" : usuario, "password" : password};
-	//var cont = $("#"+lugarCarga);
-	  	$.ajax({
-	        data:  queryParams,
-	        url:   'ajax.php',
-	        type:  'post',
-	        success:  function (response) { 
+			var usuario = $('#usuario').val();
+			var password = $('#password').val();
 
-	        	if(response.length > 0){
-	        		alert(response);
-	        		//TODO nice notification
-	        	}else{
-	        		top.location.href = 'index.php';
-	        	}
-	        }
-		});
+			var queryParams = { "func" : 'logIn', "usuario" : usuario, "password" : password};
+				$.ajax({
+				data:  queryParams,
+				url:   'ajax.php',
+				type:  'post',
+				success:  function (response) { 
+
+				        if(response.length > 0){
+				        		notificaError(response);
+				        }else{
+				        		top.location.href = 'index.php';
+				        }
+				    }
+				});
+			}
+
 	}
+
+	//resetea password
+	function resetar(){
+
+		var usuario = $('#usuarioRecuperacion').val();
+		var email = $('#emailRecuperacion').val();
+			
+		if(usuario != ''){
+
+			var queryParams = { "func" : 'resetPasswordUsuario', "usuario" : usuario};
+			$.ajax({
+				data:  queryParams,
+				url:   'ajax.php',
+				type:  'post',
+				success:  function (response) { 
+
+					if(response.length == 0){
+
+						notifica('Se ha enviado un email con el password temporal.');
+						return;
+
+					}else{
+					    notificaError(response);
+					}
+				}
+			});
+
+		}
+		if(email != ''){
+			
+			var queryParams = { "func" : 'resetPasswordEmail', "email" : email};
+			$.ajax({
+				data:  queryParams,
+				url:   'ajax.php',
+				type:  'post',
+				success:  function (response) { 
+
+				    if(response.length == 0){
+
+				        notifica('Se ha enviado un email con el password temporal.');
+				        return;
+				    }else{
+				        notificaError(response);
+				    }
+				        
+				}
+			});
+		}
+
+	}
+
+	/*
+		NOTIFICACIONES
+	*/
+
+	//usa noty (jquery plugin) para notificar 
+	function notifica(text) {
+	  	var n = noty({
+	  		text: text,
+	  		type: 'alert',
+	    	dismissQueue: true,
+	  		layout: 'topCenter',
+	  		closeWith: ['click'], // ['click', 'button', 'hover']
+	  	});
+	  	console.log('html: '+n.options.id);
+	  	
+	  	//tiempo para desaparecerlo solo 
+	  	setTimeout(function (){
+			n.close();
+		},3000);
+	}
+
+	//notifica errores
+	function notificaError(text) {
+	  	var n = noty({
+	  		text: text,
+	  		type: 'error',
+	    	dismissQueue: true,
+	  		layout: 'topCenter',
+	  		closeWith: ['click'], // ['click', 'button', 'hover']
+	  	});
+	  	console.log('html: '+n.options.id);
+	  	
+	  	//tiempo para desaparecerlo solo 
+	  	setTimeout(function (){
+			n.close();
+		},3000);
+	}
+
 	</script>
 </head>
 
@@ -105,15 +222,40 @@ if( isset($_SESSION['logueado']) ){
 
 		<div id="usuarios">
 			<div class="titulo">Ingresar</div>
-			<div class="contenido">
-				<input type="text" class="validate[required]" placeholder="Usuario" required="required" id="usuario" name="usuario"><br/>
-				<input type="password" class="validate[required]" required="required" placeholder="Password" id="password" name="password"><br/>
-				
-				<span class=".boton" onClick="loginbox(1)">Registrarse</span>
-				<input type="submit" value="Entrar" name="entrar" id="entrar"><br/>
+			<div class="contenido" id="login">
+				<input type="text" class="validate[required,custom[onlyLetterSp]]" placeholder="Usuario" id="usuario" name="usuario"><br/>
 
+				<input type="password" class="validate[required]" placeholder="Password" id="password" name="password"><br/>
+				
+			</div>
+
+			<!-- recuperacion -->
+			<br/>
+			<span id="recuperacion" onClick="formRecuperacion()">¿Has olvidado tu contraseña?</span>
+			<br/>
+			<br/>
+
+			<div id="formRecuperacion">
+				<input type="text" class="validate[optional,custom[onlyLetterSp]]" placeholder="Usuario" id="usuarioRecuperacion" name="usuarioRecuperacion">
+				
+				<br/>
+				O
+				<br/>
+
+				<input type="email" class="validate[optional,custom[email]]" placeholder="Email" id="emailRecuperacion" name="emailRecuperacion">
+			</div>
+			<!-- end formRecuperacion -->
+
+			<div class="controls">
+
+				<input type="submit" style="margin-right:-140px;" onClick="logIn()" id="entrar" value="Entrar">
+
+				<span style="margin-right:-140px;" onClick="resetar()" id="resetear">Resetear</span>
+
+				<span style="float:right;" onClick="loginbox(1)">Registrarse</span>
 			</div>
 		</div>
+		<!-- end usuarios -->
 
 		<div id="nuevoUsuario" >
 			<div class="titulo">
@@ -121,19 +263,22 @@ if( isset($_SESSION['logueado']) ){
 			</div>
 			<div class="contenido">
 
-				<input type="text" class="validate[required]" required="required" placeholder="Usuario" name="nuevoUsuario"><br/>
+				<input type="text" class="validate[required]" placeholder="Usuario" name="nuevoUsuario"><br/>
 
-				<input type="email" class="validate[required,custom[email]]" required="required" placeholder="Email" name="nuevoEmail"><br/>
+				<input type="email" class="validate[required,custom[email]]" placeholder="Email" name="nuevoEmail"><br/>
 
-				<input class="validate[required]" required="required" id="nuevoPassword1" placeholder="Password" name="nuevoPassword1" type="password"><br/>
+				<input class="validate[required]" id="nuevoPassword1" placeholder="Password" name="nuevoPassword1" type="password"><br/>
 
-				<input class="validate[required,equals[nuevoPassword1]]" required="required" placeholder="Confirmar password" name="nuevoPassword2" type="password"><br/>
-
-				<span class=".boton" onClick="loginbox(2)">Usuarios</span>
-				<input type="submit" value="Registrarse" name="registrar"><br/>
+				<input class="validate[required,equals[nuevoPassword1]]" placeholder="Confirmar password" name="nuevoPassword2" type="password"><br/>
 
 			</div>
-		</div>
+
+			<div class="controls">
+				<input style="margin-right:-112px;" type="submit" value="Registrarse" name="registrar">
+				<span style="float:right;	" onClick="loginbox(2)">Usuarios</span>
+			</div>
+
+		</div><!-- end nuevoUsuario -->
 
 	</form>
 
