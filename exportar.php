81a572412ab589ac606292069b872568182efdc0
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-11-05 16:49:56 -0600

    version 0.9.5 ESTABLE
    Casi lista, solo falta testeo en el servidor, exporta con un tema de
    colores de la marca.
    Pone imagen del logo en la parte inferior derecha.

--- exportar.php
+++ exportar.php
@@ -34,7 +34,6 @@ function exportar(){
 	$columnaTitulo = 'style="background-color: blue; color: #fff; font-bold: bold; text-align: center;"';
 	$columna = 'style="text-align: left;"';
 	$reporteInfo = 'style="background-color: red; color: #fff;"';
-	$columnaLogo = 'style="text-align: center;"';
 	$logo = 'style="width: 200px;"';
 
 	$sql = 'SELECT * FROM proyectos WHERE cliente = '.$_SESSION['id'];
@@ -45,11 +44,11 @@ function exportar(){
 	//rellena exell
 	echo '<table>
 		<tr>
-			<td colspan="4" '.$titulo.'>Proyectos</td>
+			<td colspan="6" '.$titulo.'>Proyectos</td>
 		</tr>
 		<tr>
 			<td '.$columnaTitulo.'>Nombre</td>
-			<td '.$columnaTitulo.'>Descripcion</td>
+			<td colspan="3"'.$columnaTitulo.'>Descripcion</td>
 			<td '.$columnaTitulo.'>Fecha</td>
 			<td '.$columnaTitulo.'>Estado</td>
 		</tr>';
@@ -58,7 +57,7 @@ function exportar(){
 		
 		echo '<tr>
 			<td '.$columna.'>'.$row['nombre'].'</td>
-			<td '.$columna.'>'.$row['descripcion'].'</td>
+			<td colspan="3"'.$columna.'>'.$row['descripcion'].'</td>
 			<td '.$columna.'>'.$row['fecha'].'</td>
 			<td '.$columna.'>';
 
@@ -74,7 +73,7 @@ function exportar(){
 
 	echo '
 		<tr>
-			<td colspan="4" '.$reporteInfo.'> Generado Automaticamente el '.date("d-m-Y").'
+			<td colspan="6" '.$reporteInfo.'> Generado Automaticamente el '.date("d-m-Y").'
 			</td>
 		</tr>
 		</table>';
@@ -90,24 +89,27 @@ function exportarProyecto($id){
 	$columnaTitulo = 'style="background-color: #a1ca4a; color: #fff; font-bold: bold; font-size: 16pt; text-align: center;"';
 	$columna = 'style="text-align: left; font-size: 14pt;"';
 	$tituloInfo = 'style="background-color: #a1ca4a; color: #fff; font-size: 16pt; text-align: center;"';
-	$columnanInfo = 'style="background-color: #757374; color: #fff; font-size: 14pt; text-align: left;"';
+	$columnanInfo = 'style="background-color: #f4f4f4; color: #757374; font-size: 14pt; text-align: left;"';
+	$logo = 'style="background-color: #f4f4f4; color: #757374; font-size: 14pt; text-align: center;"';
 	
 	//nombre por defecto, despues lo cambia
 	$nombre = 'proyecto'.$id;
 	$cuerpo = '';
-	$home = 'http://localhost/Matrices/index.php';
+	$detalles = '';//informacion detallada de las categorias, normas y sus detalles
+	$home = 'http://localhost/Matrices/';
 
 	$sql = 'SELECT * FROM proyectos WHERE cliente = '.$_SESSION['id'].' AND id = '.$id;
 	$result = mysql_query($sql);
 
 	header("Content-Type: application/vnd.ms-excel");
 
+	//crea el resumen del proyecto
 	while( $row = mysql_fetch_array($result) ){
 		$nombre = $row['nombre'];
 
 		$cuerpo .= '<tr>
 			<td '.$columna.'>'.$row['nombre'].'</td>
-			<td '.$columna.'>'.$row['descripcion'].'</td>
+			<td colspan="3"'.$columna.'>'.$row['descripcion'].'</td>
 			<td '.$columna.'>'.$row['fecha'].'</td>
 			<td '.$columna.'>';
 
@@ -124,42 +126,123 @@ function exportarProyecto($id){
 	//encabezados del exell
 	$encabezado = '<table '.$tabla.'>
 		<tr>
-			<td colspan="4" '.$titulo.'>Resumen de '.$nombre.'</td>
+			<td colspan="6" '.$titulo.'>Resumen de '.$nombre.'</td>
 		</tr>
 		<tr>
 			<td '.$columnaTitulo.'>Nombre</td>
-			<td '.$columnaTitulo.'>Descripcion</td>
+			<td colspan="3"'.$columnaTitulo.'>Descripcion</td>
 			<td '.$columnaTitulo.'>Fecha</td>
 			<td '.$columnaTitulo.'>Estado</td>
 		</tr>';
 
 	//informacion del informe
 	$footer = '<tr>
-			<td colspan="4" '.$tituloInfo.'> Generado Automaticamente</td>
+			<td colspan="6" '.$tituloInfo.'> Generado Automaticamente</td>
 			</tr>';
 
 	$footer .= '<tr>
 				<td '.$columnanInfo.'>Fecha:</td>
-				<td colspan="3" '.$columnanInfo.'>'.date("F j Y - g:i a").'</td>';
+				<td colspan="4" '.$columnanInfo.'>'.date("F j Y - g:i a").'</td>
+				<td rowspan="3" colspan="1" '.$logo.'>
+					<img style="text-align: center; vertical-align: center; margin: 0 auto;" src="http://localhost/Matrices/images/logoExcel.png">
+				</td>';
 
 	$footer .= '<tr>
 				<td '.$columnanInfo.'>Por:</td>
-				<td colspan="3" '.$columnanInfo.'>'.$_SESSION['nombre'].'</td>
+				<td colspan="5" '.$columnanInfo.'>'.$_SESSION['nombre'].'</td>
 				</tr>';
 
 	$footer .= '<tr>
 			<td '.$columnanInfo.'>Generado en:</td>
-			<td colspan="3" '.$columnanInfo.'> 
-				<a style="color: #fff;" href="'.$home.'">Escala.com</a>
+			<td colspan="5" '.$columnanInfo.'> 
+				<a href="'.$home.'">Escala.com</a>
 			</td>
 			</tr>
 		</table>';
 
+	//crea detalles del proyecto
+	$detalles = detalles($id);
+
 	//imprime el archivo 
-	echo $encabezado.$cuerpo.$footer;
+	echo $encabezado.$cuerpo.$detalles.$footer;
 
 	//descarga el archivo
 	header("Content-disposition: attachment; filename=".$nombre.".xls");
+}
+
+/*
+	crea la tabla de detalles
+	@param return $detalle 
+*/
+
+function detalles($id){
+	//estilos
+	$titulo = 'style="background-color: #6fa414; font-bold: bold; color: #fff; font-size: 18pt; text-align: center;"';
+	$columnaTitulo = 'style="background-color: #a1ca4a; color: #fff; font-bold: bold; font-size: 16pt; text-align: center;"';
+	$tituloCategoria = 'style="background-color: #757374; font-bold: bold; color: #fff; font-size: 18pt; text-align: center;"';
+	$columna = 'style="text-align: left; font-size: 14pt;"';
+
+	$detalles = '
+	<tr>
+		<td colspan="6" '.$titulo.'>GENERALIDADES</td>
+	</tr>
+	<tr>
+		<td '.$columnaTitulo.'>N DE NORMA</td>
+		<td '.$columnaTitulo.'>NOMBRE DE NORMA</td>
+		<td '.$columnaTitulo.'>REQUISITO LEGAL</td>
+		<td '.$columnaTitulo.'>RESUMEN</td>
+		<td '.$columnaTitulo.'>PERSIMOS</td>
+		<td '.$columnaTitulo.'>ENTIDAD COMPETENTE</td>
+	</tr>';
+
+	$sql = 'SELECT DISTINCT categoria FROM registros WHERE proyecto = '.$id;
+	$result = mysql_query($sql);
+
+	while($row = mysql_fetch_array($result)){
+
+		$detalles .= infoCategoria($row['categoria']);
+
+		$sql2 = 'SELECT DISTINCT norma FROM registros WHERE categoria = '.$row['categoria'];
+		$result2 = mysql_query($sql2);
+		
+		while($row2 = mysql_fetch_array($result2)){
+			$detalles .= infoNorma($row2['norma']);
+		}
+	}
+
+	return $detalles;
+}
+
+//para el titulo de la categoria
+function infoCategoria($categoria){
+	$tituloCategoria = 'style="background-color: #f4f4f4; font-bold: bold; color: #757374; font-size: 18pt; text-align: center;"';
+
+	$sql = 'SELECT * FROM categorias WHERE id = '.$categoria;
+	$result = mysql_query($sql);
+	$row = mysql_fetch_array($result);
+	//titulo
+	return '<tr><td colspan="6" '.$tituloCategoria.'>'.$row['nombre'].'</td></tr>';
+}
+
+//devuelve la informacion de todas las normas de una categoria
+function infoNorma($id){
+	$columna = 'style="text-align: left; font-size: 14pt; border: 1px solid #f4f4f4;"';
+
+	$info = '';
+	$sql = 'SELECT * FROM normas WHERE id = '.$id;
+	$result = mysql_query($sql);
+	
+	$row = mysql_fetch_array($result);
+		$info .= '<tr>';
+		$info .= '<td '.$columna.'>'.$row['numero'].'</td>';
+		$info .= '<td '.$columna.'>'.$row['nombre'].'</td>';
+		$info .= '<td '.$columna.'>'.$row['resumen'].'</td>';
+		$info .= '<td '.$columna.'>'.$row['requisito'].'</td>';
+		$info .= '<td '.$columna.'>'.$row['permisos'].'</td>';
+		$info .= '<td '.$columna.'>'.$row['entidad'].'</td>';
+		$info .= '</tr>';
+
+	return $info;
 }
 
 ?>
