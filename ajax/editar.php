c5781a7cdcd701743fc4047c7d7f164d283d4ca2
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-11-12 11:03:12 -0600

    vista de edicion  Desarrollo v 0.9.8.6
    vista de edicion de proyctos

---
+++ ajax/editar.php
@@ -0,0 +1,95 @@
+<?php
+	require_once("../db.php"); 
+/*
+	vista para editar, seleccionar y cargar informacion de las categorias seleccionadas en el proyecto
+*/
+
+//logueo -> seguridad por si el usuario no esta logueado
+
+if( !isset($_SESSION['logueado']) ){
+	$home = $_SESSION['home']."/login.php";
+	header('Location: '.$home);
+	exit;
+}
+
+
+if( isset($_GET['proyecto']) && isset($_GET['categoria']) ){
+	//muestra vista de edicion
+	editar($_GET['proyecto'], $_GET['categoria']);
+}
+
+
+//muestra vista de edicion
+// @param proyecto -> id del proyecto
+// @param categoria -> id de la categoria seleccionada
+function editar($proyecto, $categoria){
+
+	//si se ha seleccionado una categoria
+	if($categoria == 0){
+		//no se ha seleccionado categoria
+		echo '<div>Selecciona o agrega una Categoria para editar</div>';
+	}else{
+		//edicion
+		?>
+		<div id="nivel1">
+
+				<div id="listaNormas">
+					
+				</div>
+				<div id="generalidades">
+					
+				</div>
+
+			</div><!-- end nivel 1-->
+
+			<div id="nivel2">
+				<div id="columna1">
+					<!-- -->
+					<div id="descripcionNorma">
+						
+						<div class="nombreNorma">
+							TODO titulo ajax categoria
+						</div>
+						<div>
+							TODO descripcion
+						</div>
+						
+					</div>
+					<!-- -->
+				</div> <!-- end columna1-->
+
+				<div id="columna2">
+					<!-- -->
+					<div class="box">
+						TODO ajax para mostrar informacion de subcategorias<br/>
+						TODO mansory para acomodar las columnas
+					</div>
+					<div class="box">
+						TODO ajax para mostrar informacion de subcategorias<br/>
+						TODO mansory para acomodar las columnas
+						<br/>
+						<br/>
+					</div>
+					<div class="box">
+						TODO ajax para mostrar informacion de subcategorias<br/>
+						TODO mansory para acomodar las columnas
+						<br/>
+						<br/>
+						<br/>
+						<br/>
+					</div>
+					<div class="box">
+						TODO ajax para mostrar informacion de subcategorias<br/>
+						TODO mansory para acomodar las columnas
+					</div>
+					<!-- MODELO PARA BOX -->
+					
+				</div><!--end columna2 -->
+
+			</div><!-- end nivel 2-->
+		<?php
+	}
+}
+
+
+?>
