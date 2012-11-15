5e207b2d1d72abeaff24c2a6dc81994c880b93be
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-11-15 11:33:07 -0600

    Desarrollo
    administra usuario logueo y deslogueo

---
+++ usuario.php
@@ -0,0 +1,32 @@
+<?php
+
+class Usuario{
+
+	public function __construct(){
+		session_start();
+	}
+
+	/**
+	* SE ENCARGA DE LOGUEAR USUARIO
+	*/
+	public function login(){
+
+	}
+
+	/**
+	* LOGOUT PARA USUARIO
+	*/
+	public function logout(){
+
+	}
+
+	/**
+	* GETTER PARA NOMBRE DE USUARIO
+	* return nombre
+	*/
+	public function getNombre(){
+		
+	}
+}
+
+?>
